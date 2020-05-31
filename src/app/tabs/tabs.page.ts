274edcdfe33838f01ae61f5b4425aaa44d90a963
Manohisoa Randriambahiny <randriambahinymanohy@gmail.com>
2020-05-31 13:10:03 +0300

    s

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Location } from "@angular/common";
+import { ApiService } from '../services/api.service';
+
 
 @Component({
   selector: 'app-tabs',
@@ -8,10 +11,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
-  
-  constructor(private router:Router) { }
+  url:string;
+  constructor(private router:Router,private api:ApiService,private location: Location) { }
  
   ngOnInit() {
+    this.router.events.subscribe(val => {
+      if (this.location.path() != "") {
+        this.url = this.location.path().split('/')[2];
+      } 
+    });
+   
   }
   addDep(){
     this.router.navigateByUrl('/tabs/add-depense');
