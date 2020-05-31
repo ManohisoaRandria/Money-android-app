8ed7363ce544dbf21076a6fc52b8923d2379ec7d
RAMANANTSEHENO Tahiana <ramanantsehenotahiana@gmail.com>
2020-05-31 15:02:10 +0300

    modif bouton deco

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 import { Location } from "@angular/common";
 import { ApiService } from '../services/api.service';
@@ -12,8 +13,8 @@ import { ApiService } from '../services/api.service';
 })
 export class TabsPage implements OnInit {
   url:string;
-  constructor(private router:Router,private api:ApiService,private location: Location) { }
- 
+  constructor(private router:Router,private api:ApiService,private location: Location,public alertControlle:AlertController) { }
+
   ngOnInit() {
     this.router.events.subscribe(val => {
       if (this.location.path() != "") {
@@ -22,9 +23,30 @@ export class TabsPage implements OnInit {
     });
    
   }
-  
+
   addDep(){
     this.router.navigateByUrl('/tabs/add-depense');
+  }
+
+  //deco
+  async onLogOut(){
+    const alert = await this.alertControlle.create({
+      header: 'Are you sure?',
+      buttons: [
+        {
+          text: 'No'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.router.navigate(['auth']);
+            console.log('ok');
+          }
+        }
+    ]
+    });
+
+    await alert.present();
   }
 
 }
