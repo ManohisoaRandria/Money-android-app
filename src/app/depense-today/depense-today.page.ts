7642e9fc340ec35320f4439e3bc4d0e39630fd4f
patrickrasabonihantana2 <patrickrasabonihantana2@gmail.com>
2020-05-31 11:09:12 +0300

    depense today

--- src/app/depense-today/depense-today.page.ts
+++ src/app/depense-today/depense-today.page.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./depense-today.page.scss'],
 })
 export class DepenseTodayPage implements OnInit {
-
+  items = [
+    {
+      montant: 1000,
+      date: new Date('2020-5-30'),
+      motif: 'cigarette',
+      motifVisible: false
+    },
+    {
+      montant: 2500,
+      date: new Date('2020-5-30'),
+      motif: 'gouter',
+      motifVisible: false
+    },
+    {
+      montant: 500,
+      date: new Date('2020-5-30'),
+      motif: 'frais',
+      motifVisible: false
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  viewMotif(item: any) {
+    if(!item.motifVisible) {
+      item.motifVisible = true;
+    } else {
+      item.motifVisible = false;
+    }
   }
 
 }
