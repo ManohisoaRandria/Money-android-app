79ae803ed299a6b77f86cf2d1889f4db3ecacc2f
RAMANANTSEHENO Tahiana <ramanantsehenotahiana@gmail.com>
2020-06-01 12:18:44 +0300

    getMySolde

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Platform } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -42,7 +45,7 @@ export class ApiService {
       const requestOptions = {                                                                                                                                                                                 
         headers: header
       };
-      this.http.get('http://localhost/volakoBack/depense/all',requestOptions).subscribe((res:any[])=>{
+      this.http.get('http://localhost/wccs2-volako-back/depense/all',requestOptions).subscribe((res:any[])=>{
           if(typeof(res)=='string' && res=="not loged"){
             reject(res);
           }else{
@@ -66,7 +69,7 @@ export class ApiService {
       const requestOptions = {                                                                                                                                                                                 
         headers: header
       };
-      this.http.get('http://localhost/volakoBack/depense/today',requestOptions).subscribe((res:any[])=>{
+      this.http.get('http://localhost/wccs2-volako-back/depense/today',requestOptions).subscribe((res:any[])=>{
           if(typeof(res)=='string' && res=="not loged"){
             reject(res);
           }else{
@@ -93,7 +96,7 @@ export class ApiService {
       const requestOptions = {                                                                                                                                                                                 
         headers: header
       };
-      this.http.post('http://localhost/volakoBack/mp',data,requestOptions).subscribe(res=>{
+      this.http.post('http://localhost/wccs2-volako-back/mp',data,requestOptions).subscribe(res=>{
           if(res=="data invalid" || res=="no data to insert"){
             reject(res);
           }else{
@@ -106,12 +109,69 @@ export class ApiService {
                 resolve(rep);
               });
             });
-            
+            ///Pour modifier le solde dans home
+            this.getMySolde().then(res=>{
+              resolve(res);
+            });
           }
       },error=>{
         reject(error);
       })
     });
-   
+  }
+
+  getMySolde(){
+    return new Promise((resolve,reject)=>{
+      const header=new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+localStorage.getItem('token')
+      });
+      const requestOptions = {                                                                                                                                                                                 
+        headers: header
+      };
+      this.http.get('http://localhost/wccs2-volako-back/mySolde',requestOptions).subscribe((res:any)=>{
+          if(res=="not loged"){
+            reject(res);
+          }else{
+            this.solde = res;
+            this.soldeSubjectDepense();
+            resolve(res);
+          }
+      },error=>{
+        reject(error);
+      })
+    });
+  }
+
+    onAddSold(montant:Number,date:Date){
+      return new Promise((resolve,reject)=>{
+      var datePipe = new DatePipe("en-US");
+      let dateS = datePipe.transform(date, 'yyyy/MM/dd');
+      let data={
+          "montant":montant,
+          "date":dateS
+      }
+      //header
+      const optionRequete = {
+          headers: new HttpHeaders({ 
+            'Authorization':'Bearer '+localStorage.getItem("token"),
+          })
+      };
+      this.http.post('http://localhost/wccs2-volako-back/credit',data,optionRequete).subscribe(res=>{
+          if(res == "not loged" || res =="no data to insert" || res == "data invalid"){
+            reject("Credit failed");
+          }else{
+             ///Pour modifier le solde dans home
+            this.getMySolde().then(res=>{
+              resolve(res);
+            });
+            console.log(localStorage.getItem('user'));
+            resolve("Credit succes");
+          } 
+      },error=>{
+        reject(error);
+      })
+      
+    });
   }
 }
--- src/app/services/credit.service.ts
+++ src/app/services/credit.service.ts
@@ -14,36 +14,4 @@ import { DatePipe } from '@angular/common';
 export class CreditService {
 
      constructor(private http:HttpClient){}
-
-     onAddSold(montant:Number,date:Date){
-        return new Promise((resolve,reject)=>{
-        var datePipe = new DatePipe("en-US");
-        let dateS = datePipe.transform(date, 'yyyy/MM/dd');
-
-        let data={
-            "montant":montant,
-            "date":dateS
-        }
-
-        //header
-        const optionRequete = {
-            headers: new HttpHeaders({ 
-              'Authorization':'Bearer '+localStorage.getItem("token"),
-            })
-        };
-
-        this.http.post('http://localhost/wccs2-volako-back/credit',data,optionRequete).subscribe(res=>{
-            if(res == "not loged" || res =="no data to insert" || res == "data invalid"){
-              reject("Credit failed");
-            }else{
-                console.log(res);
-                resolve("Credit succes");
-            } 
-        },error=>{
-          reject(error);
-        })
-        
-     });
-     
-    }
 }
